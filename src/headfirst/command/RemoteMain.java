071578338cd2bc6763350c13f1809936a16fabae
Hoonjin(Desktop) <bwjhj1030@gmail.com>
2015-08-12 22:36:48 +0900

    메인함수에 해시맵을 사용하여 일일히 인덱스를 기억하지 않아도 위치로 명령을 실행할 수 있게 함

--- src/headfirst/command/RemoteMain.java
+++ src/headfirst/command/RemoteMain.java
@@ -1,13 +1,21 @@
 package headfirst.command;
 
+import java.util.HashMap;
+
 public class RemoteMain {
 
     public static void main(String[] args) {
         String[] locations = {"거실", "부엌", "욕실", "방1", "방2"};
+
+        HashMap<String, Integer> mapper = new HashMap<String, Integer>();
+        for (int i = 0; i < locations.length; i++) {
+            mapper.put(locations[i], i);
+        }
+
         RemoteController controller = new RemoteController(locations.length);
         controller.addCommands(locations);
 
-        controller.onButtonPushed(1);
-        controller.offButtonPushed(1);
+        controller.onButtonPushed(mapper.get("거실"));
+        controller.offButtonPushed(mapper.get("거실"));
     }
 }
