4cd0530c4753dcdd43eea95181d7c8390c01e491
HoonJin(Notebook) <bwjhj1030@gmail.com>
2015-08-17 16:26:05 +0900

    Tea 클래스를 만들고 메인에서 메인에서 prepareBeverage메서드를 각각 호출. 각 클래스의 brew함수가 동작한다.

--- src/headfirst/templatemethod/BeverageMain.java
+++ src/headfirst/templatemethod/BeverageMain.java
@@ -3,7 +3,10 @@ package headfirst.templatemethod;
 public class BeverageMain {
 
     public static void main(String[] args) {
-        Coffee coffee = new Coffee();
+        Beverage coffee = new Coffee();
         coffee.prepareBeverage();
+
+        Beverage tea = new Tea();
+        tea.prepareBeverage();
     }
 }
---
+++ src/headfirst/templatemethod/Tea.java
@@ -0,0 +1,9 @@
+package headfirst.templatemethod;
+
+public class Tea extends Beverage {
+
+    @Override
+    public void brew() {
+        System.out.println("brewing Tea~~");
+    }
+}
