5f22f0450df9d6f20039a7568395d0e1091fd594
Hoonjin(Desktop) <bwjhj1030@gmail.com>
2015-08-21 16:07:07 +0900

    State 인터페이스를 구현하는 NoQuarter를 구현 & HasQuarter는 몸체만 만들어 놓음  & GumballMachine에 각 스테이트 변수들을 만들고 각 메서드들은 스테이트에 메서드를 사용하도록 함

--- src/headfirst/state/GumballMachine.java
+++ src/headfirst/state/GumballMachine.java
@@ -2,7 +2,51 @@ package headfirst.state;
 
 public class GumballMachine {
 
-    public GumballMachine() {
+    private State noQuarterState;
+    private State hasQuarterState;
+    private int numberOfGumballs;
 
+    private State currentState;
+
+    public GumballMachine(int numberOfGumballs) {
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+
+        if (numberOfGumballs > 0) {
+            currentState = noQuarterState;
+        }
+    }
+
+    public void setState(State state) {
+        currentState = state;
+    }
+
+    public void insertQuarter() {
+        currentState.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        currentState.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        currentState.turnCrank();
+        currentState.dispense();
+    }
+
+    public void releaseBall() {
+        System.out.println("A Gumball comes rolling out the slot....");
+
+        if (numberOfGumballs > 0) {
+            numberOfGumballs -= 1;
+        }
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
     }
 }
